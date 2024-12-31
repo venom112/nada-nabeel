4ba8a32081126ec46fd29fe66356599b6ded9cec
Mohammad 404 <51339765+venom112@users.noreply.github.com>
2024-12-31 22:14:21 +0200

    Add files via upload

---
+++ stud-Login.js
@@ -0,0 +1,41 @@
+
+
+
+
+document.getElementById("button-nav1").onclick=function(){
+    document.getElementById("poup").style.display="block"
+}
+document.getElementById("img-in-poup").onclick=function(){
+    document.getElementById("poup").style.display="none"
+}
+
+// 
+
+document.getElementById("button-nav2").onclick=function(){
+    document.getElementById("poup-2").style.display="block"
+}
+document.getElementById("img-in-poup-2").onclick=function(){
+    document.getElementById("poup-2").style.display="none"
+}
+
+// Initialization for ES Users
+import { Dropdown, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown, Ripple });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
